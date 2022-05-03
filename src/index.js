be8664e35d123be4484f83ddfba9e27a5b7f00df
Naveen <naveenofficial100@gmail.com>
2022-05-03 15:37:26 +0530

    thirteenth commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<App />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
