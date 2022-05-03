423c6b31a7917cf220e0821127a25ada649bf8c9
Naveen <naveenofficial100@gmail.com>
2022-05-03 12:03:59 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import "./App.css";
-import data from "./data";
+import React from "react";
 import Post from "./Post";
+import Avatar from "@mui/material/Avatar";
+import { useEffect, useState } from "react";
+import data from "./data";
 
 function App() {
+  // console.log(data[0].posts);
+
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+
+  // })
+
   return (
-    <div className="App">
-      <div className="parenddiv">
-        <div className="childdiv">
-          {data.map((values, index) => {
-            return <Post key={index} values={values} />;
+    <div className="app">
+      <div className="app__posts">
+        <div className="app__posts_left">
+          {data[0].posts.map((values, index) => {
+            return <Post key={index} values={values} userdata={data[0]} />;
           })}
+        </div>
+        <div className="app__posts_right">
+          <div className="app__posts_right__header">
+            <Avatar
+              className="post__avatar"
+              alt="Remy Sharp"
+              src={data[0].picture}
+            />
+            <div>
+              <p>
+                <strong>mr_naveen_143</strong>
+              </p>
+              <p>Naveen Lagishetty</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./Profile";
+import Appheader from "./Appheader";
+import Createpost from "./Createpost";
+import Signup from "./Signup";
+import Signin from "./Signin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/signup" element={<Signup />} />
+
+        <Route exact path="/signin" element={<Signin />} />
+
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Appheader />
+              <App />
+            </>
+          }
+        />
+
+        <Route
+          exact
+          path="/profile"
+          element={
+            <>
+              <Appheader />
+              <Profile />
+            </>
+          }
+        />
+
+        <Route
+          exact
+          path="/createpost"
+          element={
+            <>
+              <Appheader />
+              <Createpost />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
