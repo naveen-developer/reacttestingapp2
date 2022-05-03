f793691164378e0f0f5ed8e164d0bc8c7e5315a0
Naveen <naveenofficial100@gmail.com>
2022-05-03 10:56:32 +0530

    first commit

---
+++ src/Post.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <div>
+      <h2>Vijay poster 1</h2>
+      <img src="/images/vijay1.jpg" width="100%" alt="image" />
+    </div>
+  );
+};
+
+export default Post;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    name: "vijay post 1",
+    image: "/images/vijay1.jpg",
+  },
+  {
+    name: "vijay post 2",
+    image: "/images/vijay2.jpg",
+  },
+  {
+    name: "vijay post 1",
+    image: "/images/vijay2.jpg",
+  },
+  {
+    name: "vijay post 1",
+    image: "/images/vijay2.jpg",
+  },
+  {
+    name: "vijay post 1",
+    image: "/images/vijay2.jpg",
+  },
+];
